3e9aee76480257eff500df70261a0439f821c2a6
ludani <ldanivieira@gmail.com>
2021-02-25 19:01:49 -0400

    componentes angular-cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jad-root', //NOME DO COMPONENTE
+  templateUrl: './app.component.html', // HTML DO MEU TEMPLADE
+
+})
+export class AppComponent {
+  title = 'jedi-academy';
+}
