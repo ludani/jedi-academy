0490ed7a8fa6d1a559b62df4b1b6fa813aba416a
ludani <ldanivieira@gmail.com>
2021-02-26 21:51:46 -0400

    finalizando a aplicação jedi academy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Student } from './student/student.model';
 
 @Component({
   selector: 'jad-root', //NOME DO COMPONENTE
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
 
 })
 export class AppComponent {
-  title = 'jedi-academy';
+
+  students: Student[] = [
+    {name: "Luke", isJedi: true, temple:'Coruscant'},
+    {name: "Han Solo", isJedi: false},
+    {name: "Leia", isJedi: false}
+  ]
+
 }
